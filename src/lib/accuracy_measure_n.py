f3ad4ec6870e6bac1d065b398b91b86999d51803
Niyas C <niyasc@localhost.localdomain>
2013-12-13 00:06:06 +0530

    Ensemble with varrying voting power

--- src/lib/accuracy_measure_n.py
+++ src/lib/accuracy_measure_n.py
@@ -7,9 +7,8 @@ from clean import getList
 from random import randint
 from random import seed
 from math import log
-def naive_bayes(freq_list,database,t):
+def naive_bayes(freq_list,database):
 	#database=pickle.load(open('database.db','rb'))
-	#print(database)
 	#print(type(database))
 	
 	v=0	
@@ -17,11 +16,7 @@ def naive_bayes(freq_list,database,t):
 		for word in database[category]:
 			v+=database[category][word]
 			
-	'''modification'''
-	wc=0
-	for word in freq_list:
-		wc+=freq_list[word]
-	'''modification calc ends'''
+
 	
 	pc={}
 	for category in categories:
@@ -36,9 +31,9 @@ def naive_bayes(freq_list,database,t):
 		
 		for word in freq_list:
 			if word not in attributes:
-				pc[category]=pc[category]+log(((1.0/(n+v))))+log(freq_list[word]/float(wc))
+				pc[category]=pc[category]+log(((1.0/(n+v))))
 			else:
-				pc[category]=pc[category]+log(((1.0+attributes[word]/(n+v))))+log(freq_list[word]/float(wc))
+				pc[category]=pc[category]+log(((1.0+attributes[word]/(n+v))))
 				
 				
 	'''for category in categories:
@@ -53,6 +48,19 @@ def naive_bayes(freq_list,database,t):
 			p=pc[category]
 	
 	return Category
+
+def makedb(train_set):
+	database={}
+	for category in categories:
+		database[category]={}
+		for document in train_set[category]:
+			freq=getList('./dataset/'+category+'/'+document)
+			for word in freq:
+				if word not in database[category]:
+					database[category][word]=freq[word]
+				else:
+					database[category][word]+=freq[word]
+	return database
 
 def accuracy_measure_n(n):
 	'''n->number of train documents'''
@@ -88,34 +96,66 @@ def accuracy_measure_n(n):
 				if d not in train_set[category]:
 					test_set[category].append(d)
 		print("Traing and test sets created")
+		#divide train set into 3
+		tset0={}
+		tset1={}
+		tset2={}
+		for category in train_set:
+			tset0[category]=train_set[category][0:int(n/3)]
+			tset1[category]=train_set[category][int(n/3):int(2*n/3)]
+			tset2[category]=train_set[category][int(2*n/3):]
 		#train the model
-		database={}
-		#number of train documents nt
-		nt=0
-		for category in categories:
-			database[category]={}
-			nt+=len(train_set[category])
-			for document in train_set[category]:
-				freq=getList('./dataset/'+category+'/'+document)
-				for word in freq:
-					if word not in database[category]:
-						database[category][word]=freq[word]
-					else:
-						database[category][word]+=freq[word]
+		database0=makedb(tset0)
+		database1=makedb(tset1)
+		database2=makedb(tset2)
+		
 		print("training completed n,i",n,i)		
 #test model
+		vp={0:1,1:1,2:1}
 		#number of train documents
 		for category in categories:
 			p=0
 			j=0;
-			t=len(train_set[category])/float(nt)
 			for document in test_set[category]:
 				j+=1
 				freq=getList('./dataset/'+category+'/'+document)
-				p_cat=naive_bayes(freq,database,t)
+				pc={}
+				
+				c0=naive_bayes(freq,database0)
+				if c0 in pc:
+					pc[c0]+=vp[0]
+				else:
+					pc[c0]=vp[0]
+				c1=naive_bayes(freq,database1)
+				if c1 in pc:
+					pc[c1]+=vp[1]
+				else:
+					pc[c1]=vp[1]
+				c2=naive_bayes(freq,database2)
+				if c2 in pc:
+					pc[c2]+=vp[2]
+				else:
+					pc[c2]=vp[2]
+				#collect majority vote	
+				p_cat=max(pc, key=pc.get)
 				if p_cat==category:
 					p+=1
-				print('n',n,'i',i,'accuracy',(p/float(j))*100,'category',category)
+					
+				#change voting power
+				if c0==category:
+					vp[0]*=1.01
+				else:
+					vp[0]*=0.99
+				if c1==category:
+					vp[1]*=1.01
+				else:
+					vp[1]*=0.99
+				if c2==category:
+					vp[2]*=1.01
+				else:
+					vp[2]*=0.99
+				
+				#print('n',n,'i',i,'j',j,'accuracy',(p/float(j))*100,'category',category,vp[0],vp[1],vp[2])
 				if p_cat=='Unable to decide':
 					print("unable to decide happens")
 			f=open("status.txt","a")
